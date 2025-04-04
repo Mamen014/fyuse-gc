6c41f902707e1843e63fef157de6481a20212c69
Mamen014 <ryaniaska14@gmail.com>
2025-04-04 19:58:26 +0700

    remove the session.js

---
+++ .next/server/pages/api/auth/session.js
@@ -0,0 +1,5 @@
+const CHUNK_PUBLIC_PATH = "server/pages/api/auth/session.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/[root of the server]__2ab5d801._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/auth/session.js [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/auth/session.js [api] (ecmascript)\" } [api] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
